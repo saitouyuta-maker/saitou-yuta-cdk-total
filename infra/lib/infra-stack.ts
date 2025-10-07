d852167da5626d1836f99ab4e92acf8019634fad
yuta <saitou.yuta@simpline.co.jp>
2025-10-07 16:11:10 +0900

    Add test Dockerfile for API connectivity

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -566,7 +566,7 @@ export class InfraStack extends cdk.Stack {
         command: [
           "python3",
           "-c",
-          "import requests; print('Start JSONPlaceholder test'); r=requests.get('https://jsonplaceholder.typicode.com/todos/1'); print(r.json()); import time; time.sleep(3600)"
+          "import requests,time; print('Start JSONPlaceholder test'); r=requests.get('https://jsonplaceholder.typicode.com/todos/1'); print(r.json()); time.sleep(3600)"
         ],
       }
     );
